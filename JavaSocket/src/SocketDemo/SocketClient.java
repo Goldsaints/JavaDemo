bfb2d1cd62e562092e5f928b428e1461e093c5d4
Goldsaints <710845067@qq.com>
2018-05-06 22:04:45 +0800

    update SocketClient,SocketServer

--- JavaSocket/src/SocketDemo/SocketClient.java
+++ JavaSocket/src/SocketDemo/SocketClient.java
@@ -1,4 +1,5 @@
 package SocketDemo;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
@@ -9,10 +10,55 @@ public class SocketClient {
 		int port = 3333;
 		//与服务端建立连接
 		Socket socket = new Socket(host, port);
+		
 		//连接后，接收输出流
 		OutputStream outputStream = socket.getOutputStream();
 		String message = "Hello! ben";
-		socket.getOutputStream().write(message.getBytes("UTF-8"));
+
+//    没有发送完毕标志的发送信息方法
+//		socket.getOutputStream().write(message.getBytes("UTF-8"));
+		
+		/**
+		 * 2个字节表示长度，告诉服务端发送的信息有多长，然后服务端知道接收多长的信息就表示接收完毕
+		 */
+		//计算发送消息的长度
+		byte[] sendBytes = message.getBytes("UTF-8");
+		System.out.println(sendBytes.length+"--"+(sendBytes.length >> 8));
+		outputStream.write(sendBytes.length >> 8);
+		outputStream.write(sendBytes.length);
+		//然后将信息再次发送出去
+		outputStream.write(sendBytes);
+		outputStream.flush();
+		
+		 //==========此处重复发送一次，实际项目中为多个命名，此处只为展示用法
+	    message = "the second message!";
+	    sendBytes = message.getBytes("UTF-8");
+	    outputStream.write(sendBytes.length >>8);
+	    outputStream.write(sendBytes.length);
+	    outputStream.write(sendBytes);
+	    outputStream.flush();
+	    
+	  //==========此处重复发送一次，实际项目中为多个命名，此处只为展示用法
+	    message = "the third message!";
+	    sendBytes = message.getBytes("UTF-8");
+	    outputStream.write(sendBytes.length >>8);
+	    outputStream.write(sendBytes.length);
+	    outputStream.write(sendBytes);  
+		
+//		//通过shutdownOutput高速服务器已经发送完数据，后续只能接受数据
+		socket.shutdownOutput();
+//		从服务端接收信息	
+		InputStream inputStream = socket.getInputStream();
+		byte[] bytes =new byte[1024];
+		int len ; 		
+		StringBuilder sb = new StringBuilder();
+		while((len = inputStream.read(bytes))!=-1) {
+			sb.append(new String(bytes, 0 , len , "UTF-8"));
+		}
+		System.out.println("get message from server "+sb);
+		
+	
+		inputStream.close();
 		outputStream.close();
 		socket.close();
 	}
--- JavaSocket/src/SocketDemo/SocketServer.java
+++ JavaSocket/src/SocketDemo/SocketServer.java
@@ -1,6 +1,8 @@
 package SocketDemo;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,16 +17,47 @@ public class SocketServer {
 		//server将一直等待连接的到来
 		System.out.println("server将一直等待连接的到来");
 		Socket socket = server.accept();
-		//建立链接后，从socket中获取输入流，并建立缓冲区进行读取
+		
+		//建立链接后，从socket中获取输入流，并建立缓冲区进行读取（接收客户端发送的消息）
 		InputStream inputStream = socket.getInputStream();
 		byte[] bytes = new byte[1024];
-		int len;
-		StringBuilder sb = new StringBuilder();
-		while((len = inputStream.read(bytes))!=-1) {
-			//注意制定编码格式，发送方和接收方一定要统一，建议使用UTF-8
-			sb.append(new String(bytes,0,len,"UTF-8"));
+//		int len;
+//		StringBuilder sb = new StringBuilder();
+//		while((len = inputStream.read(bytes))!=-1) {
+//			//注意制定编码格式，发送方和接收方一定要统一，建议使用UTF-8
+//			sb.append(new String(bytes,0,len,"UTF-8"));
+//		}
+//		System.out.println("get message from client : "+sb);
+		
+		/**
+		 * 2个字节表示长度，告诉服务端发送的信息有多长，然后服务端知道接收多长的信息就表示接收完毕
+		 * 1个字节：最大256，表示256B
+		 * 2个字节：最大65536，表示64K
+		 * 3个字节：最大16777216，表示16M
+		 * 4个字节：最大4294967296，表示4G
+		 */
+		while(true) {
+			//首先获取前两个字节表示的长度
+			int first = inputStream.read();
+			//如果读到-1，表示读到流的结尾，socket已经关闭，此时将不能再去读取
+			if(first == -1) {
+				break;
+			}
+			int second = inputStream.read();
+			int length = (first << 8)+second;
+			//然后构造一个指定长的byte数组
+			bytes  = new byte[length];
+			//然后读取指定长度的消息即可
+			inputStream.read(bytes);
+			System.out.println("get message from client: "+new String(bytes, "UTF-8"));
 		}
-		System.out.println("get message from client : "+sb);
+		
+		//不带发送结束符号的发送信息方法
+//		OutputStream outputStream = socket.getOutputStream();
+//		outputStream.write("Hello Client! I get the message.".getBytes("UTF-8"));
+//		outputStream.close();
+		
+		
 		inputStream.close();
 		socket.close();
 		server.close();
